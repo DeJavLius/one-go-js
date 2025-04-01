fa60fee7c31a13782f1a670ae697a12d3a250bc0
DeJavLiusMac <lmo9903@gmail.com>
2025-04-02 00:12:50 +0900

    function early return pattern

--- Test/src/index.js
+++ Test/src/index.js
@@ -1,1 +1,45 @@
-console.log("start VSCode");
+// typeof 해당 변수의 자료형 출력
+
+// let num = 10; number
+// num = 10.001; number
+// num = infinity; number 10/0
+// num = NaN; number 'abc'/0
+
+//BigInt
+
+// BigInt() or BigNum = 99999999999999999n
+
+// Early Return Pattern
+
+function compare(num) {
+  if (num == 0) {
+    console.log("0");
+  } else if (num < 0) {
+    console.log("-1");
+  }
+  else {
+    if (num >= 10) {
+      console.log("10");
+    } else {
+      console.log("0<>10");
+    }
+  }
+}
+
+// 먼저 return을 통해 바로 처리됨
+function better_compare(num) {
+  if (num === 0) {
+    return "";
+  }
+  if (num < 0) {
+    return "-1";
+  }
+  if (num >= 10) {
+    return "10";
+  }
+  
+  return console.log("0<>10");
+}
+
+compare(15);
+console.log(better_compare(13));
